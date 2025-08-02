ba4a883039743e332dc24413a2527cb8722ea481
Sterben <sergey.matushkin.98@mail.ru>
2025-08-02 13:10:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  const hundreds = 'hundred';
+
+  if (number < 20) {
+    return ones[number];
+  }
+
+  if (number < 100) {
+    const tenPart = Math.floor(number / 10);
+    const onePart = number % 10;
+    return `${tens[tenPart]}${onePart > 0 ? ` ${ones[onePart]}` : ''}`;
+  }
+
+  if (number < 1000) {
+    const hundredPart = Math.floor(number / 100);
+    const rest = number % 100;
+    return `${ones[hundredPart]} ${hundreds}${rest > 0 ? ` ${toReadable(rest)}` : ''}`;
+  }
+
+  throw new Error('Number must be less than 1000');
 };
